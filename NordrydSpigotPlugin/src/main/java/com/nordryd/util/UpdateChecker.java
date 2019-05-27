31dc61635d64ba668b59e2f6693898793ee3f908
Jacob Overton <32399688+nordryd@users.noreply.github.com>
2019-05-26 20:43:17 -0500

    Spigot (#1)
    * Try to make PlayerInteractEvent work next. Lookup youtube tutorial
    
    * Print statements replaced with getLogger().info(...)

--- NordrydSpigotPlugin/src/main/java/com/nordryd/util/UpdateChecker.java
+++ NordrydSpigotPlugin/src/main/java/com/nordryd/util/UpdateChecker.java
@@ -1,6 +1,5 @@
 package com.nordryd.util;
 
-import static java.lang.System.out;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,22 +7,30 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class UpdateChecker extends Thread
 {
-	URLConnection conn = null;
+	private final JavaPlugin plugin;
+	
+	public UpdateChecker(JavaPlugin plugin) {
+		this.plugin = plugin;
+	}
 	
 	@Override
 	public void run() {
+		URLConnection conn = null;
+		
 		try {
 			conn = new URL(Reference.API_LINK).openConnection();
 			
 			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			
 			if(reader.readLine().equals(Reference.CURRENT_VERSION)) {
-				out.println(Values.PREFIX + " No updates available.");
+				plugin.getLogger().info("No updates available atm.");
 			}
 			else {
-				out.println(Values.PREFIX + " There is an update! Download it at " + Reference.RESOURCE_URL);
+				plugin.getLogger().info("There's an update! Download it at " + Reference.RESOURCE_URL);
 			}
 		}
 		catch(MalformedURLException mue) {
